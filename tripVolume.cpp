effc2fc7fa891c61403267165e19d35666635c94
tingfan@unimatrx <tingfan@gmail.com>
2014-10-27 06:46:31 -0500

    make viz OSX compatible (GUI from mainthread)

--- tripVolume.cpp
+++ tripVolume.cpp
@@ -1,6 +1,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/openni2_grabber.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/octree/octree.h>
 #include <pcl/filters/extract_indices.h>
@@ -18,8 +19,11 @@ enum
 class OpenNIChangeViewer
 {
 	public:
+  		typedef pcl::PointCloud<pcl::PointXYZRGBA> Cloud;
+		typedef Cloud::ConstPtr CloudConstPtr;
 		OpenNIChangeViewer (double resolution, int mode, int noise_filter)
-			: viewer ("PCL OpenNI Viewer")
+//			: viewer ("PCL OpenNI Viewer")
+    		: viewer (new pcl::visualization::PCLVisualizer ("PCL OpenNI2 cloud"))
 		{
 			octree = new pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGBA>(resolution);
 			mode_ = mode;
@@ -49,29 +53,25 @@ class OpenNIChangeViewer
 				switch (mode_) 
 				{
 					case REDDIFF_MODE:
+						//modify those new point to red
 						filtered_cloud.reset (new pcl::PointCloud<pcl::PointXYZRGBA> (*cloud));
 						filtered_cloud->points.reserve(newPointIdxVector->size());
 
 						for (std::vector<int>::iterator it = newPointIdxVector->begin (); it != newPointIdxVector->end (); it++)
 							filtered_cloud->points[*it].rgba = 255<<16;
-
-						if (!viewer.wasStopped())
-							viewer.showCloud (filtered_cloud);
-
 						break;
 					case ONLYDIFF_MODE:
 						filtered_cloud.reset (new pcl::PointCloud<pcl::PointXYZRGBA>);
-
 						filtered_cloud->points.reserve(newPointIdxVector->size());
 
 						for (std::vector<int>::iterator it = newPointIdxVector->begin (); it != newPointIdxVector->end (); it++)
 							filtered_cloud->points.push_back(cloud->points[*it]);
 
 
-						if (!viewer.wasStopped())
-							viewer.showCloud (filtered_cloud);
 						break;
 				}
+				boost::mutex::scoped_lock lock (cloud_mutex_);
+				cloud_ = filtered_cloud;
 
 				// switch buffers - reset tree
 				octree->switchBuffers ();
@@ -91,18 +91,49 @@ class OpenNIChangeViewer
 				pcl::io::OpenNI2Grabber::Mode image_mode = pcl::io::OpenNI2Grabber::OpenNI_Default_Mode;
 				pcl::io::OpenNI2Grabber grabber_ (device_id, depth_mode, image_mode);
 
-				boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f = 
-					boost::bind (&OpenNIChangeViewer::cloud_cb_, this, _1);
+				boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f = boost::bind (&OpenNIChangeViewer::cloud_cb_, this, _1);
 				boost::signals2::connection c = grabber_.registerCallback (f);
+
+				viewer->setCameraFieldOfView (1.02259994f);
+
+				bool cloud_init = false;
 
     			grabber_.start ();
 				//interface->start ();
-				while (!viewer.wasStopped())
+				while (!viewer->wasStopped())
 				{
-					boost::this_thread::sleep(boost::posix_time::seconds(1));
+					viewer->spinOnce();
+					CloudConstPtr cloud;
+					if (cloud_mutex_.try_lock ())
+					{
+						cloud_.swap (cloud);
+						cloud_mutex_.unlock ();
+					}
+					if(cloud)
+					{
+						//viewer.showCloud (cloud);
+						if (!cloud_init)
+						{
+							viewer->setPosition (0, 0);
+							viewer->setSize (cloud->width, cloud->height);
+							cloud_init = !cloud_init;
+						}
+
+						if (!viewer->updatePointCloud (cloud, "OpenNICloud"))
+						{
+							viewer->addPointCloud (cloud, "OpenNICloud");
+							viewer->resetCameraViewpoint ("OpenNICloud");
+							viewer->setCameraPosition (
+									0,0,0,		// Position
+									0,0,1,		// Viewpoint
+									0,-1,0);	// Up
+						}
+					}
 				}
     			grabber_.stop ();
 				//interface->stop ();
+
+				c.disconnect();
 
 
 
@@ -110,10 +141,14 @@ class OpenNIChangeViewer
 			}
 
 		pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGBA> *octree;
-		pcl::visualization::CloudViewer viewer;
+		//pcl::visualization::CloudViewer viewer;
+  		boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
 
 		int mode_;
 		int noise_filter_;
+
+		boost::mutex cloud_mutex_;
+		CloudConstPtr cloud_;
 };
 
 	int 
